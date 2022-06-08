bf0ab703ce4e47c882d43944f2cb652ddb1c3108
BrendenSeifried <brendenseifried@gmail.com>
2022-06-08 11:12:21 -0700

    setup with some logic in controllers

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,8 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+Drop table if exists cats;
+
+CREATE table cats (
+    id BIGINT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR NOT NULL
+);
