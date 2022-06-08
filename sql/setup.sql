d8c27133366c7ca17ca2b22bc84bd1f3ebd14f38
BrendenSeifried <brendenseifried@gmail.com>
2022-06-08 12:20:16 -0700

    all names showing with id in beekeeper

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,11 +1,24 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+
 Drop table if exists cats;
 
 CREATE table cats (
     id BIGINT GENERATED ALWAYS AS IDENTITY,
-    name VARCHAR NOT NULL
+    name VARCHAR NOT NULL,
+    -- type VARCHAR NOT NULL 
 );
 
-INSERT INTO cats (name) VALUES ('Tiger');
+INSERT INTO cats (name) VALUES ('Felix'), ('Garfield'), ('Duchess'), ('Stimpy'), ('Sylvester'), ('Tigger'), ('Hello Kitty'), ('Hobbes') ;
+
+INSERT INTO cats (type) VALUES ('Tuxedo'), ('Orange Tabby'), ('Angora'), ('Manx'), ('Tuxedo'), ('Tiger'), ('Angora'), ('Tiger') ;
+
+-- INSERT INTO cats (url) VALUES ('Felix'), ('Garfield'), ('Duchess'), ('Stimpy'), ('Sylvester'), ('Tigger'), ('Hello Kitty'), ('Hobbes') ;
+
+-- INSERT INTO cats (year) VALUES ('Felix'), ('Garfield'), ('Duchess'), ('Stimpy'), ('Sylvester'), ('Tigger'), ('Hello Kitty'), ('Hobbes') ;
+
+-- INSERT INTO cats (lives) VALUES ('Felix'), ('Garfield'), ('Duchess'), ('Stimpy'), ('Sylvester'), ('Tigger'), ('Hello Kitty'), ('Hobbes') ;
+
+-- INSERT INTO cats (isSideKick) VALUES ('Felix'), ('Garfield'), ('Duchess'), ('Stimpy'), ('Sylvester'), ('Tigger'), ('Hello Kitty'), ('Hobbes') ;
+
 
